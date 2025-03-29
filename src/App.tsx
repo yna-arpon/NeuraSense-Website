912aa2ca362a1f5f087fa58701ce27442ddad056
Yna Arpon <ynaarpon4@gmail.com>
2025-03-29 17:24:50 -0600

    Clear app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import Illustration from "./components/Illustration";
 
 import "./App.css";
 
 const App = () => {
-  return (
-    <>
-      <div id="container">
-        <div style={{ textAlign: "center" }}>
-          <Illustration />
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
