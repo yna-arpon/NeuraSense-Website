33ade9c3e56b1a10e9e10fe8bf90eac805a8fa06
Yna Arpon <ynaarpon4@gmail.com>
2025-03-31 15:25:10 -0600

    Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import "./App.css";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <Header></Header>
+      <Hero></Hero>
     </>
   );
 };
---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import styles from "./Hero.module.css";
+
+import hero from "../assets/hero.png";
+
+const Hero = () => {
+  return (
+    <>
+      <div id={styles.heroContainer}>
+        <h1 id={styles.heroText}>
+          Revolutionizing Stroke Detection for Children on Life Support
+        </h1>
+        <img src={hero} id={styles.heroImg}></img>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
